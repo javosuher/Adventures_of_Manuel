3af4879f8458b978d784eda6db2e5974fd66d82a
marianufon <maria.nufon@gmail.com>
2014-01-10 12:38:50 +0100

    Nivel 5 con todos los elementos

--- AventuresOfManuel project/AdventuresOfManuel/src/com/me/adventures/screens/Nivel5.java
+++ AventuresOfManuel project/AdventuresOfManuel/src/com/me/adventures/screens/Nivel5.java
@@ -3,7 +3,7 @@ package com.me.adventures.screens;
 import com.badlogic.gdx.math.Vector2;
 import com.me.adventures.characters.*;
 import com.me.adventures.main.AdventuresOfManuel;
-import com.me.adventures.main.Constant;
+
 
 public class Nivel5 extends Nivel {
 	public Nivel5(AdventuresOfManuel adventures, Vector2 posicionManuel) {
@@ -12,7 +12,6 @@ public class Nivel5 extends Nivel {
 	
 	@Override
 	protected void mapaDelNivel() {
-		mapaNivel.add(new Puente(new Vector2(193, 348),Constant.PUENTE_VERTICAL));
 	}
 	
 	@Override
@@ -20,76 +19,69 @@ public class Nivel5 extends Nivel {
 		//Se introducen las paredes del nivel
 		objetos.add(new Pared(new Vector2(0, 0), 1024, 58));
 		objetos.add(new Pared(new Vector2(135, 0), 58, 754));
-		objetos.add(new Pared(new Vector2(135, 696), 348, 58));
-		objetos.add(new Pared(new Vector2(483, 725), 58, 29));
-		salida = new Salida(new Vector2(425, 696), "PUERTA");
-		objetos.add(new Pared(new Vector2(541, 696), 290, 58));
+		objetos.add(new Pared(new Vector2(0, 696), 1024, 58));
+		salida = new Salida(new Vector2(483, 116), "ESCALERA");
 		objetos.add(new Pared(new Vector2(831, 0), 58, 754));
 		
 		//Se introducen los demas objetos por fila
-		objetos.add(new Roca(new Vector2(251, 580)));
-		objetos.add(new Roca(new Vector2(309, 580)));
-		objetos.add(new Roca(new Vector2(367, 580)));
-		objetos.add(new Roca(new Vector2(425, 580)));
-		objetos.add(new Roca(new Vector2(483, 580)));
-		objetos.add(new Roca(new Vector2(541, 580)));
-		objetos.add(new Bloque(new Vector2(599, 580)));
-		
-		objetos.add(new Bloque(new Vector2(309, 522)));
+		objetos.add(new Roca(new Vector2(193, 638)));
+		objetos.add(new Roca(new Vector2(251, 638)));
+		objetos.add(new Roca(new Vector2(309, 638)));
+		objetos.add(new Roca(new Vector2(367, 638)));
+		objetos.add(new Bloque(new Vector2(599, 638)));
+		objetos.add(new Roca(new Vector2(773, 638)));
+
+		objetos.add(new Roca(new Vector2(193, 580)));
+		corazones.add(new Corazon(new Vector2(251, 580), 0));
+		objetos.add(new Bloque(new Vector2(367, 580)));
+		objetos.add(new Roca(new Vector2(599, 580)));
+		corazones.add(new Corazon(new Vector2(657, 580), 0));
+		objetos.add(new Roca(new Vector2(715, 580)));
+		objetos.add(new Roca(new Vector2(773, 580)));
+
+		objetos.add(new Roca(new Vector2(193, 522)));
+		objetos.add(new Roca(new Vector2(251, 522)));
+		objetos.add(new Roca(new Vector2(367, 522)));
+		objetos.add(new Roca(new Vector2(599, 522)));
+		objetos.add(new Roca(new Vector2(657, 522)));
 		objetos.add(new Roca(new Vector2(715, 522)));
-		
-		objetos.add(new Roca(new Vector2(541, 464)));
-		objetos.add(new Roca(new Vector2(599, 464)));
+		objetos.add(new Roca(new Vector2(773, 522)));
+
 		objetos.add(new Roca(new Vector2(657, 464)));
 		objetos.add(new Roca(new Vector2(715, 464)));
+		objetos.add(new Roca(new Vector2(773, 464)));
 		
-		corazones.add(new Corazon(new Vector2(193, 406), 0));
-		objetos.add(new Agua(new Vector2(251, 406)));
-		objetos.add(new Agua(new Vector2(309, 406)));
-		objetos.add(new Agua(new Vector2(367, 406)));
-		objetos.add(new Agua(new Vector2(425, 406)));
-		objetos.add(new Roca(new Vector2(541, 406)));
-		corazones.add(new Corazon(new Vector2(599, 406), 0));
-		objetos.add(new Bloque(new Vector2(715, 406)));
+		cofre = new Cofre(new Vector2(483, 348), 4);
 
-		objetos.add(new Agua(new Vector2(251, 348)));
-		objetos.add(new Agua(new Vector2(309, 348)));
-		objetos.add(new Agua(new Vector2(367, 348)));
-		cofre = new Cofre(new Vector2(425, 348), 5);
-		objetos.add(new Roca(new Vector2(541, 348)));
-		corazones.add(new Corazon(new Vector2(599, 348), 0));
-		objetos.add(new Bloque(new Vector2(715, 348)));
+		objetos.add(new Roca(new Vector2(193, 232)));
+		objetos.add(new Bloque(new Vector2(251, 232)));
+		objetos.add(new Roca(new Vector2(309, 232)));
 
-		corazones.add(new Corazon(new Vector2(193, 290), 0));
-		objetos.add(new Agua(new Vector2(251, 290)));
-		objetos.add(new Agua(new Vector2(309, 290)));
-		objetos.add(new Agua(new Vector2(367, 290)));
-		objetos.add(new Agua(new Vector2(425, 290)));
-		objetos.add(new Roca(new Vector2(541, 290)));
-		corazones.add(new Corazon(new Vector2(599, 290), 0));
-		objetos.add(new Bloque(new Vector2(715, 290)));
-		
-		objetos.add(new Roca(new Vector2(541, 232)));
-		objetos.add(new Roca(new Vector2(599, 232)));
-		objetos.add(new Roca(new Vector2(657, 232)));
-		objetos.add(new Roca(new Vector2(715, 232)));
+		objetos.add(new Roca(new Vector2(193, 174)));
+		objetos.add(new Roca(new Vector2(309, 174)));
+		objetos.add(new Roca(new Vector2(367, 174)));
+		objetos.add(new Roca(new Vector2(599, 174)));
+		objetos.add(new Roca(new Vector2(657, 174)));
+		objetos.add(new Bloque(new Vector2(715, 174)));
+		objetos.add(new Roca(new Vector2(773, 174)));
 
-		objetos.add(new Bloque(new Vector2(309, 174)));
-		objetos.add(new Roca(new Vector2(715, 174)));
-		
-		objetos.add(new Roca(new Vector2(251, 116)));
-		objetos.add(new Roca(new Vector2(309, 116)));
-		objetos.add(new Roca(new Vector2(367, 116)));
-		objetos.add(new Roca(new Vector2(425, 116)));
-		objetos.add(new Roca(new Vector2(483, 116)));
-		objetos.add(new Roca(new Vector2(541, 116)));
-		objetos.add(new Bloque(new Vector2(599, 116)));
+		objetos.add(new Bloque(new Vector2(367, 116)));
+		objetos.add(new Roca(new Vector2(599, 116)));
+		corazones.add(new Corazon(new Vector2(657, 116), 0));
+		objetos.add(new Roca(new Vector2(773, 116)));
+
+		objetos.add(new Roca(new Vector2(193, 58)));
+		corazones.add(new Corazon(new Vector2(251, 58), 0));
+		objetos.add(new Roca(new Vector2(309, 58)));
+		objetos.add(new Roca(new Vector2(367, 58)));
+		objetos.add(new Roca(new Vector2(599, 58)));
+		objetos.add(new Roca(new Vector2(657, 58)));
+		objetos.add(new Roca(new Vector2(773, 58)));
 	}
 	
 	@Override
 	protected void personajesDelNivel() {
-		personajes.add(new Serpiente(new Vector2(367, 638), manuel));//calavera
-		personajes.add(new Serpiente(new Vector2(657, 348), manuel));//calavera
-		personajes.add(new Serpiente(new Vector2(367, 58), manuel));//calavera
+		personajes.add(new Serpiente(new Vector2(367, 348), manuel));//Fantasma
+		personajes.add(new Serpiente(new Vector2(599, 348), manuel));//Fantasma
 	}
 }
